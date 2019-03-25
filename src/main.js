0cf93c0579dc1d9d348096125d23ccc47c19d20d
vanyo <jokerwanr@gmail.com>
2019-03-25 17:28:47 +0800

    banner

--- src/main.js
+++ src/main.js
@@ -1,19 +1,33 @@
 //入口文件
 import Vue from 'vue'
-
+//安装路由
+import VueRouter from 'vue-router'
+//安装路由
+Vue.use(VueRouter)
 // 按需导入Mint-UI中的组件
-import { Header } from 'mint-ui';
+import { Header, Swipe, SwipeItem } from 'mint-ui';
 import 'mint-ui/lib/style.css'
 Vue.component(Header.name, Header);
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
+
+//导入vue-resource
+import VueResource from 'vue-resource'
+Vue.use(VueResource)
 
 //导入MUI的样式
 import './lib/mui/css/mui.min.css'
 import './lib/mui/css/icons-extra.css'
+
+//导入自己的router.js模块
+import router from './router.js'
+
 //导入App根组件
 import app from './App.vue'
 
 
 var vm = new Vue({
     el:"#app",
-    render: c => c(app)
+    render: c => c(app),
+    router //挂在路由对象到vm实例上
 })
--- src/router.js
+++ src/router.js
@@ -1,10 +1,21 @@
+//安装路由
 import VueRouter from 'vue-router'
 
+//导入对应的路由组件
+import HomeContainer from './components/tabbar/HomeContainer.vue'
+import MemberContainer from './components/tabbar/MemberContainer.vue'
+import ShopContainer from './components/tabbar/ShopContainer.vue'
+import SearchContainer from './components/tabbar/SearchContainer.vue'
 
 var router = new VueRouter({
     routes: [
-        
-    ]
+        //配置路由规则
+        { path: '/home', component: HomeContainer},
+        { path: '/member', component: MemberContainer },
+        { path: '/shop', component: ShopContainer },
+        { path: '/search', component: SearchContainer }
+    ],
+    linkActiveClass:'mui-active' //覆盖默认的router-link-active类
 })
 
 export default router
